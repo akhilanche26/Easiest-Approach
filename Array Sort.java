6e31f7e2e74886002f9de1da937d494890be7d9b
akhilanche26 <118436639+akhilanche26@users.noreply.github.com>
2024-05-20 12:08:59 +0530

    Add files via upload

---
+++ Array Sort.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class MyComp implements Comparator<Integer>{
+    public int compare(Integer i1, Integer i2){
+        if(i1<i2) return 1;
+        if(i1>i2) return -1;
+        return 0;
+    }
+}
+public class Sample{
+    public static void main(String[] args){
+        Integer[] a = {2,4,6,8,1,3,5,7}; //array declare Integer because custom comparator works only with objects.
+        Arrays.sort(a,new MyComp());
+        System.out.println(Arrays.toString(a));
+    }
+}
