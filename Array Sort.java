e452be75f689bbe174a77247a24e54ce82bf359d
akhilanche26 <118436639+akhilanche26@users.noreply.github.com>
2024-06-14 15:58:32 +0530

    Update Array Sort.java

--- Array Sort.java
+++ Array Sort.java
@@ -8,7 +8,7 @@ class MyComp implements Comparator<Integer>{
 }
 public class Sample{
     public static void main(String[] args){
-        Integer[] a = {2,4,6,8,1,3,5,7}; //array declare Integer because custom comparator works only with objects.
+        Integer[] a = {2,4,6,8,1,3,5,7}; //array declared as Integer because custom comparator works only with objects.
         Arrays.sort(a,new MyComp());
         System.out.println(Arrays.toString(a));
     }
